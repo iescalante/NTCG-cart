fa49fdcfe37c50ccd01144fa54b25ec86c92051c
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-12-03 21:38:38 -0500

    hide https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const dotenv = require("dotenv");
-const https = require("https");
+// const https = require("https");
 dotenv.config();
 
 const express = require("express");
